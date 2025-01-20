5bba713c07a586f89018d22c7ff7fd6119b814b9
Eduardo-Ennes <eduardosimoes9899@gamil.com>
2025-01-19 21:32:05 -0300

    saudacao add

--- git_github.py
+++ git_github.py
@@ -9,11 +9,21 @@ for c in range(num, 0, -1):
     valor = valor * c
 print(valor)
 
+print()
+name = str(input('Digite o seu nome: '))
+print(f'Seja bem vindo {name}')
+
+
+
 
 '''
+passDesteCurso testegithub
 git init
+
+git add . && git commit -m "nome"
 git add "nome.py"
 git commit "nome"
+
 git log
 git log --oneline
 git checkout id ou nome 
@@ -25,12 +35,5 @@ git branch nome
 git branch -d nome -> se tiver commit não deleta 
 git branch -D nome -> Deleta tudo e de qualquer forma
 git merge nome
-
-
-
-CONFIGURAÇÃO DA SSH:
-ssh-keygen -t ed25519 -C "seu_email@example.com"
-git remote -v
-git remote remove origin
-
+git push -u origin main
 '''
